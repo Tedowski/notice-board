b4f8b3c93e1af884b208adf58754b3e455b5cac5
Tadeas Kosek <tako@ageras.com>
2023-07-24 13:35:17 +0200

    feat: POC

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import PostService from "./services/PostService";
+import { useQuery } from "react-query";
 
 function App() {
+  const { data } = useQuery('posts', async () => {
+    return (await PostService.list()).data
+  })
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>🏝</div>
   );
 }
 
